d82267a701e02e0e17604d9a5301dbea91c936e2
vonckenj <154799923+vonckenj@users.noreply.github.com>
2023-12-25 14:48:01 +0100

    check db

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-echo "Hello Azure!";
+$servername = "gymbook-server.mysql.database.azure.com";
+$username = "npfxzsjaxr";
+$password = "PKT58F3662KKRBTN$";
+$dbname = "gymbook-server";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Query to get all tables in the database
+$sql = "SHOW TABLES";
+$result = $conn->query($sql);
+
+// Check if there are any tables
+if ($result->num_rows > 0) {
+    echo "Tables in the database: <br>";
+    while ($row = $result->fetch_assoc()) {
+        echo $row["Tables_in_" . $dbname] . "<br>";
+    }
+} else {
+    echo "No tables found in the database.";
+}
+
+// Close connection
+$conn->close();
