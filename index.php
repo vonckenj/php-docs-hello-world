76670e436cf0bb631e32ea5c419d5cc7b9b227be
vonckenj <154799923+vonckenj@users.noreply.github.com>
2023-12-25 15:15:14 +0100

    S

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $password = "Ndu78Dji3nk89)9SDF";
 $dbname = "gymbook-server";
 
 $conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "/ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_ssl_set($conn,NULL,NULL, "ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, "gymbook-server.mysql.database.azure.com", "npfxzsjaxr", "Ndu78Dji3nk89)9SDF", "gymbook-server", 3306, MYSQLI_CLIENT_SSL);
 
 try {
